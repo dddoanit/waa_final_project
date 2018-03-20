3071aeb3b8fca4e62a92d28b371ee53c9c449567
Doan Danh Dat <doandanhdat852004@gmail.com>
2018-03-20 14:02:23 -0500

    update

--- airlinesWebApp/src/main/java/cs545/airline/beans/AirlineManagementBean.java
+++ airlinesWebApp/src/main/java/cs545/airline/beans/AirlineManagementBean.java
@@ -6,18 +6,28 @@ import javax.inject.Named;
 
 import cs545.airline.model.Airline;
 import cs545.airline.service.AirlineService;
+import java.io.Serializable;
 
 @Named("airlineManagementBean")
 @SessionScoped
-public class AirlineManagementBean {
+public class AirlineManagementBean implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Inject
 	private AirlineService airlineService;
 	
 	private Airline airline;
+	
+	private String name;
 
 	
 	public String create() {
+		airline = new Airline();
+		airline.setName(name);
 		this.airlineService.create(airline);
 		return "success";
 	}
@@ -30,4 +40,13 @@ public class AirlineManagementBean {
 		this.airline = airline;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
 }
