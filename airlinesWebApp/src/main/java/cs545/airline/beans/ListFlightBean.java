e9f3002c3202f4d39decea507b09d6e77f163727
Doan Danh Dat <doandanhdat852004@gmail.com>
2018-03-20 12:43:07 -0500

    update

--- airlinesWebApp/src/main/java/cs545/airline/beans/ListFlightBean.java
+++ airlinesWebApp/src/main/java/cs545/airline/beans/ListFlightBean.java
@@ -1,15 +1,23 @@
 package cs545.airline.beans;
 
+import java.io.Serializable;
 import java.util.List;
 
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import cs545.airline.model.Flight;
 import cs545.airline.service.FlightService;
 
-@Named
-public class ListFlightBean {
+@Named("listFlightBean")
+@SessionScoped
+public class ListFlightBean implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	@Inject
 	private FlightService flightService;
 	
