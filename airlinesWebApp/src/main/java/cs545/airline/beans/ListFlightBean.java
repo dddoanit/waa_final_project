ee0190bb8b9e98717f8efd4420e7fa7256413b5c
Doan Danh Dat <doandanhdat852004@gmail.com>
2018-03-20 16:42:00 -0500

    fix the command link issue when creating a new airline

--- airlinesWebApp/src/main/java/cs545/airline/beans/ListFlightBean.java
+++ airlinesWebApp/src/main/java/cs545/airline/beans/ListFlightBean.java
@@ -1,13 +1,18 @@
 package cs545.airline.beans;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import cs545.airline.model.Airplane;
+import cs545.airline.model.Airport;
 import cs545.airline.model.Flight;
+import cs545.airline.service.AirplaneService;
+import cs545.airline.service.AirportService;
 import cs545.airline.service.FlightService;
 
 @Named("listFlightBean")
@@ -18,10 +23,74 @@ public class ListFlightBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	private long airlineId;
+	
+	private Date departureDate;
+	private Date departureTime;
+	
+	private long destinationId;
+	
 	@Inject
 	private FlightService flightService;
+	
+	@Inject
+	private AirplaneService airplaneService;
+	
+	@Inject
+	private AirportService airportService;
 	
 	public List<Flight> getFlights() {
 		return flightService.findAll();
 	}
+	
+	public List<Airplane> getAirplanes() {
+		return airplaneService.findAll();
+	}
+	
+	public List<Airport> getAirports() {
+		return airportService.findAll();
+	}
+
+	public long getAirlineId() {
+		return airlineId;
+	}
+
+	public void setAirlineId(long airlineId) {
+		this.airlineId = airlineId;
+	}
+
+	public Date getDepartureDate() {
+		return departureDate;
+	}
+
+	public void setDepartureDate(Date departureDate) {
+		this.departureDate = departureDate;
+	}
+
+	public long getDestinationId() {
+		return destinationId;
+	}
+
+	public void setDestinationId(long destinationId) {
+		this.destinationId = destinationId;
+	}
+
+	public Date getDepartureTime() {
+		return departureTime;
+	}
+
+	public void setDepartureTime(Date departureTime) {
+		this.departureTime = departureTime;
+	}
+	
+	public void search() {
+		System.out.println(this.departureDate);
+		System.out.println(this.departureTime);
+		System.out.println("ListFlightBean.search()");
+	}
+	
+	public void clear() {
+		System.out.println("ListFlightBean.clear()");
+	}
+	
 }
